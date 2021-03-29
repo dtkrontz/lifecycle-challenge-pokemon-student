fac3269e03111a4a87f3d3398302f2ebc24e7315
Dtkrontz <dtkrontz@gmail.com>
2021-03-29 10:47:44 -0400

    adding class solution

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css'; 
 import PokeFetch from './Components/PokeFetch/PokeFetch';
+// import PokeFetchClass from './Components/PokeFetch/PokeFetchClass';
+
 
 
 function App() {
   return (
     <div className="App">
       <PokeFetch />
+      {/* <PokeFetchClass /> */}
     </div>
   );
 }
--- src/Components/PokeFetch/PokeFetchClass.js
+++ src/Components/PokeFetch/PokeFetchClass.js
@@ -2,17 +2,55 @@ import React, { Component } from 'react'
 import './PokeFetch.css';
 
 
-class PokeFetch extends Component {
+class PokeFetchClass extends Component {
   constructor() {
     super()
     this.state = {
       pokeInfo: '',
       pokeSprite: '',
       pokeName: '',
+
+      isRunning: false,
+      seconds: 10,
+      isVisible: false
+    }
+  }
+
+  componentDidMount() {
+    console.log('Component Mounted');
+    this.interval = setInterval(() => this.countDown(), 1000);
+    console.log(this.interval);
+  }
+
+  componentDidUpdate() {
+    console.log('Component Updated');
+    if (this.state.seconds === 0 && this.state.isVisible === false) {
+      this.setState({
+        isVisible: true
+      })
+    }
+  }
+
+  componentWillUnmount() {
+    console.log('Component Unmounted!');
+    clearInterval(this.interval);
+  }
+
+  countDown() {
+    if (this.state.isRunning === true && this.state.seconds > 0) {
+      this.setState((prevState) => ({
+        seconds: prevState.seconds - 1
+      }))
     }
   }
 
   fetchPokemon() {
+    this.setState({
+      isRunning: false,
+      isVisible: false,
+      seconds: 10,
+    })
+    
     let min = Math.ceil(1);
     let max = Math.floor(152);
     let pokeNum = Math.floor(Math.random() * (max - min) + min);
@@ -24,6 +62,7 @@ class PokeFetch extends Component {
           pokeInfo: res,
           pokeSprite: res.sprites.front_default,
           pokeName: res.species.name,
+          isRunning: true,
         })
       })
       .catch((err) => console.log(err))
@@ -33,14 +72,22 @@ class PokeFetch extends Component {
     return (
       <div className={'wrapper'}>
         <button className={'start'} onClick={() => this.fetchPokemon()}>Start!</button>
-        <h1 className={'timer'} >Timer Display</h1>
+        <h1 className={'timer'} >{this.state.seconds}</h1>
         <div className={'pokeWrap'}>
+          {this.state.isVisible ? (
           <img className={'pokeImg'} src={this.state.pokeSprite} />
-          <h1 className={'pokeName'}>{this.state.pokeName}</h1>
+          ) : (
+          <img 
+          style={{filter: 'brightness(0%)'}} 
+          className={'pokeImg'} 
+          src={this.state.pokeSprite}
+           />
+          )}
+          {this.state.isVisible ? <h1 className={'pokeName'}>{this.state.pokeName}</h1> : null} 
         </div>
       </div>
     )
   }
 }
 
-export default PokeFetch;
+export default PokeFetchClass;
